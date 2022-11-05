280e51af88a4a6a330a3e8568c001c65679e15e5
Miguel Gomez <andresmgomez23@gmail.com>
2022-11-05 11:37:11 -0400

    Finish starting hero content and reuse animations

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,25 @@
+import Link from 'next/link';
 import AnimateText from './AnimateText';
 import 'animate.css';
 
 export default function Hero() {
 	return (
-		<section className='hero-area active animate__animated animate__fadeInDown'>
-			<div className='hero-wrapper'>
-				<div className='hero-content'>
-					<h1 className='first-name'>
-						Miguel <span className='last-name'>Gomez</span>
-					</h1>
-					<h3 className='positions'>
-						<AnimateText />
-					</h3>
+		<>
+			<section className='hero-area active animate__animated animate__fadeInDown'>
+				<div className='hero-wrapper'>
+					<div className='hero-content'>
+						<h1 className='first-name'>
+							Miguel <span className='last-name'>Gomez</span>
+						</h1>
+						<h3 className='positions'>
+							<AnimateText />
+						</h3>
+						<div className='recruiter'>
+							<Link href='#resume'>Download CV</Link>
+						</div>
+					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 }

f8f4c8c0f9c7bc15fff1d237e69358d4045c7846
Miguel Gomez <andresmgomez23@gmail.com>
2022-11-05 10:45:58 -0400

    Import animate library and add rotate text effect

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,16 @@
+import AnimateText from './AnimateText';
+import 'animate.css';
+
 export default function Hero() {
 	return (
-		<section className='hero-area animated'>
+		<section className='hero-area active animate__animated animate__fadeInDown'>
 			<div className='hero-wrapper'>
 				<div className='hero-content'>
 					<h1 className='first-name'>
 						Miguel <span className='last-name'>Gomez</span>
 					</h1>
 					<h3 className='positions'>
-						<span className='suffix'>Software Engineer</span>
+						<AnimateText />
 					</h3>
 				</div>
 			</div>
